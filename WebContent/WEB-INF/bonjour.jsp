45de1b60c612c6bfaa3e09b110503b883b1fbe46
ricobsj <ricobsj@localhost>
2020-01-12 01:41:05 +0100

    Second Repo

--- WebContent/WEB-INF/bonjour.jsp
+++ WebContent/WEB-INF/bonjour.jsp
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8" />
-<title>Test</title>
-</head>
-<body>
-	<p>
-		<c:out value="Bonjour !" />
-	</p>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Test</title>
+    </head>
+    <body>
+
+        <c:if test="${ !empty nom }"><p><c:out value="Bonjour, vous vous appelez ${ nom }" /></p></c:if>
+        
+        <form method="post" action="bonjour">
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" />
+            
+            <input type="submit" />
+        </form>
+        
+    </body>
 </html>
--- src/com/octest/servlets/Test.java
+++ src/com/octest/servlets/Test.java
@@ -8,21 +8,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.octest.beans.Auteur;
-
+/**
+ * Servlet implementation class Test
+ */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	public Test() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Auteur auteur = new Auteur();
-		auteur.setPrenom("Mathieu");
-		auteur.setNom("Nebra");
-		auteur.setActif(true);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+	}
 
-		request.setAttribute("auteur", auteur);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nom = request.getParameter("nom");
+
+		request.setAttribute("nom", nom);
 
 		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
 	}
+
 }

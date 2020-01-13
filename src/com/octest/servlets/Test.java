b03aa4518cc639c9c277eb4874d54d71eee9b78e
ricobsj <ricobsj@localhost>
2020-01-13 01:16:45 +0100

    First Repo

--- src/com/octest/servlets/Test.java
+++ src/com/octest/servlets/Test.java
@@ -1,16 +1,14 @@
 package com.octest.servlets;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+
+import com.octest.bdd.Noms;
+import com.octest.beans.Utilisateur;
 
 /**
  * Servlet implementation class Test
@@ -19,9 +17,6 @@ import javax.servlet.http.Part;
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public static final int TAILLE_TAMPON = 10240;
-	public static final String CHEMIN_FICHIERS = "/Users/ricobsj/fichiers/"; // A changer
-
 	public Test() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -29,66 +24,22 @@ public class Test extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		Noms tableNoms = new Noms();
+		request.setAttribute("utilisateurs", tableNoms.recupererUtilisateurs());
 		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// On récupère le champ description comme d'habitude
-		String description = request.getParameter("description");
-		request.setAttribute("description", description);
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setNom(request.getParameter("nom"));
+		utilisateur.setPrenom(request.getParameter("prenom"));
 
-		// On récupère le champ du fichier
-		Part part = request.getPart("fichier");
+		Noms tableNoms = new Noms();
+		tableNoms.ajouterUtilisateur(utilisateur);
 
-		// On vérifie qu'on a bien reçu un fichier
-		String nomFichier = getNomFichier(part);
-
-		// Si on a bien un fichier
-		if (nomFichier != null && !nomFichier.isEmpty()) {
-			String nomChamp = part.getName();
-			// Corrige un bug du fonctionnement d'Internet Explorer
-			nomFichier = nomFichier.substring(nomFichier.lastIndexOf('/') + 1)
-					.substring(nomFichier.lastIndexOf('\\') + 1);
-
-			// On écrit définitivement le fichier sur le disque
-			ecrireFichier(part, nomFichier, CHEMIN_FICHIERS);
-
-			request.setAttribute(nomChamp, nomFichier);
-		}
+		request.setAttribute("utilisateurs", tableNoms.recupererUtilisateurs());
 
 		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
 	}
 
-	private void ecrireFichier(Part part, String nomFichier, String chemin) throws IOException {
-		BufferedInputStream entree = null;
-		BufferedOutputStream sortie = null;
-		try {
-			entree = new BufferedInputStream(part.getInputStream(), TAILLE_TAMPON);
-			sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), TAILLE_TAMPON);
-
-			byte[] tampon = new byte[TAILLE_TAMPON];
-			int longueur;
-			while ((longueur = entree.read(tampon)) > 0) {
-				sortie.write(tampon, 0, longueur);
-			}
-		} finally {
-			try {
-				sortie.close();
-			} catch (IOException ignore) {
-			}
-			try {
-				entree.close();
-			} catch (IOException ignore) {
-			}
-		}
-	}
-
-	private static String getNomFichier(Part part) {
-		for (String contentDisposition : part.getHeader("content-disposition").split(";")) {
-			if (contentDisposition.trim().startsWith("filename")) {
-				return contentDisposition.substring(contentDisposition.indexOf('=') + 1).trim().replace("\"", "");
-			}
-		}
-		return null;
-	}
 }
